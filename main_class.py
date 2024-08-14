e1107e679044076f1f40c92b7998322d8da18472
Amin-Bajelan <m.amin.bajelan@gmail.com>
2024-08-14 15:40:50 +0330

    Now you can convert morse code to text and text to morse code.

--- main_class.py
+++ main_class.py
@@ -10,12 +10,14 @@ while True:
         jason = json.load(Data)
         my_list = list(jason.keys())
         if answer == "encode" or answer == "Encode":
+
             text = input("Enter the text to be encoded: ")
             text = text.lower()
             list_text = [i for i in text]
+            print(list_text)
             for i in list_text:
                 if i == " ":
-                    empty_str = empty_str + " "
+                    empty_str = empty_str + "   "
                 else:
                     if num == 0:
                         empty_str = empty_str + jason[i]
@@ -27,14 +29,27 @@ while True:
             print(empty_str)
 
         elif answer == "decode" or answer == "Decode":
-            text = input("Enter the text to be decoded: ")
-            list_text = text.split(" ")
-            list_keys = list(jason.keys())
-            for i in list_text:
-                for j in list_keys:
-                    if i == list_keys[j]:
-                        empty_str = empty_str + jason[i]
-            print(empty_str)
+            with open("data.json", "r") as Data:
+                text = input("Enter the text to be decoded: ")
+                list_text = text.split()
+                jason = json.load(Data)
+                key_list = list(jason.values())
+                val_list = list(jason.keys())
+                empty_str = str(" ")
+                num = int(0)
+                for i in list_text:
+                    for j in range(len(key_list)):
+                        if str(i) == key_list[j]:
+                            index_con = key_list.index(i)
+                            if num == 0:
+                                empty_str = val_list[index_con]
+                            else:
+                                empty_str = empty_str + val_list[index_con]
+                            num = num + 1
+
+                print(empty_str)
+
+
 
         else:
             print("\nPlease enter a valid number. ")
